f40544465c7aaf167189437e2d78aec2f7a8bd9d
MohammadSAzar <mohazar9170@gmail.com>
2025-08-23 11:54:27 +0330

    gallery edition

--- config/urls.py
+++ config/urls.py
@@ -8,12 +8,13 @@ from dashboard.views import SaleFileGalleryView, RentFileGalleryView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    re_path(r'sale-file/gallery/(?P<slug>[-\w]+)/(?P<unique_url_id>[-\w]+)/', SaleFileGalleryView.as_view(), name='sale_file_gallery'),
-    re_path(r'rent-file/gallery/(?P<slug>[-\w]+)/(?P<unique_url_id>[-\w]+)/', RentFileGalleryView.as_view(), name='rent_file_gallery'),
+    re_path(r'sale-file/gallery/(?P<pk>[-\w]+)/(?P<unique_url_id>[-\w]+)/', SaleFileGalleryView.as_view(), name='sale_file_gallery'),
+    re_path(r'rent-file/gallery/(?P<pk>[-\w]+)/(?P<unique_url_id>[-\w]+)/', RentFileGalleryView.as_view(), name='rent_file_gallery'),
     path('__debug__/', include('debug_toolbar.urls')),
     path('dashboard/', include('django.contrib.auth.urls')),
     path('', include('dashboard.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
